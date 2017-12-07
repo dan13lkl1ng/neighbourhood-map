57a013f5668f57e7a5093749adcd885ae3a8ade7
twumm <martint.mensah@gmail.com>
2017-12-07 10:42:58 +0000

    Add locations in Accra worth visiting

---
+++ js/markers.js
@@ -0,0 +1,21 @@
+var markers = [{
+        title: 'Cafe Kwae',
+        lat: 5.601000,
+        lng: -0.177042
+    },
+    {
+        title: 'Coco Lounge',
+        lat: 5.599579,
+        lng: -0.178539
+    },
+    {
+        title: '+233 Jazz Bar & Grill',
+        lat: 5.573873,
+        lng: -0.195286
+    },
+    {
+        title: 'Zion Thai Restaurant',
+        lat: 5.559144,
+        lng: -0.177189
+    }
+];
