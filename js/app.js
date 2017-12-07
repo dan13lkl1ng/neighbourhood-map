0b66f1fefa924ffda793ab6ab8ba71000cfb3f46
twumm <martint.mensah@gmail.com>
2017-12-07 10:52:57 +0000

    Add comments on what some var and functions do

--- js/app.js
+++ js/app.js
@@ -1,18 +1,21 @@
+// Declare global variables for this application
 var map;
 
 function mapView() {
     var self = this;
-
+    // Empty markers array to load all markers from the markers.js file
+    // to be displayed on the map
     this.markers = [];
 
+    // Function to display map centered on Accra, Ghana
     this.initMap = function() {
         map = new google.maps.Map(document.getElementById('map'), {
             center: {
-                lat: 40.7413549,
-                lng: -73.9980244
+                lat: 5.603717,
+                lng: -0.186964
             },
             zoom: 13,
-            // styles: styles,
+            styles: styles,
             mapTypeControl: false
         });
     };
