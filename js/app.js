93c5592686c80d9a75becb55e85bf72db7629734
twumm <martint.mensah@gmail.com>
2017-12-08 14:55:53 +0000

    Display infowindow when a marker/location is clicked

--- js/app.js
+++ js/app.js
@@ -3,11 +3,9 @@ var map;
 
 function mapView() {
     var self = this;
-    // Empty markers array to load all markers from the markers.js file
+    // Create an empty markers array to load all markers from the markers.js file
     // to be displayed on the map
     this.markers = [];
-
-
 
     // Function to display map centered on Accra, Ghana
     this.initMap = function() {
@@ -23,9 +21,6 @@ function mapView() {
         // alert('this gets called and runs');
 
 
-        // Create an info window
-        this.largeInfowindow = new google.maps.InfoWindow();
-
         // Run through array of markers
         for (var i = 0; i < myLocations.length; i++) {
             this.locationTitle = myLocations[i].title;
@@ -40,17 +35,17 @@ function mapView() {
                     lat: this.locationLat,
                     lng: this.locationLng
                 },
-                animation: google.maps.Animation.DROP,
-                // icon: defaultIcon,
+                animation: google.maps.Animation.DROP
             });
 
             // Push the marker after each loop into the array of markers declared above
             this.markers.push(this.marker);
-            // Click listener to bounce the marker(animation)
+            // On click, bounce the marker(animation)
             this.marker.addListener('click', self.animateMarker);
-            // 
-            // this.marker.addListener('click', this.populateInfoWindow);
-
+            // On click, display the infowindow with information about the location
+            this.marker.addListener('click', function() {
+                self.populateInfoWindow(this, self.largeInfowindow);
+            });
         }
     };
 
@@ -59,21 +54,29 @@ function mapView() {
         this.setAnimation(google.maps.Animation.BOUNCE);
         setTimeout((function() {
             this.setAnimation(null);
-        }).bind(this), 1400);
+        }).bind(this), 5000);
     };
 
+
+    // Create an info window
+    this.largeInfowindow = new google.maps.InfoWindow();
+
     // This function populates the infowindow when the marker is clicked
-    // this.populateInfoWindow = function(marker, infowindow) {
-    //     if (infowindow.marker != marker) {
-    //         infowindow.setContent('<div>' + marker.title + '</div>');
-    //         infowindow.marker = marker;
-    //         // infowindow.open(map, marker);
-    //         // Close the infowindow if user clicks on the close button
-    //         infowindow.addListener('closeclick', function() {
-    //             infowindow.setMarker(null);
-    //         });
-    //     }
-    // }
+    this.populateInfoWindow = function(marker, infowindow) {
+        if (infowindow.marker != marker) {
+            infowindow.setContent('');
+            infowindow.marker = marker;
+
+            infowindow.setContent('<div id="info-window">' + marker.title + '</div>');
+            infowindow.open(map, marker);
+
+            // Close the infowindow if user clicks on the close button
+            // infowindow.addListener('closeclick', function() {
+            //     infowindow.setMarker(null);
+            // });
+        }
+
+    }
 
     // Call the map function
     this.initMap();
